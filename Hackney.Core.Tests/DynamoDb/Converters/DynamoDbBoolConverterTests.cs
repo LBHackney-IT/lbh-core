0256463a796e31dd703f82d66b14ed6d28be1943
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-05-13 11:17:56 +0100

    Added DynamoDbBoolConverter

---
+++ Hackney.Core.Tests/DynamoDb/Converters/DynamoDbBoolConverterTests.cs
@@ -0,0 +1,64 @@
+using Amazon.DynamoDBv2.DocumentModel;
+using FluentAssertions;
+using Hackney.Core.DynamoDb.Converters;
+using System;
+using Xunit;
+
+namespace Hackney.Core.Tests.DynamoDb.Converters
+{
+    public class DynamoDbBoolConverterTests
+    {
+        private readonly DynamoDbBoolConverter _sut;
+
+        public DynamoDbBoolConverterTests()
+        {
+            _sut = new DynamoDbBoolConverter();
+        }
+
+        [Fact]
+        public void ToEntryTestNullValueReturnsNull()
+        {
+            _sut.ToEntry(null).Should().BeEquivalentTo(new DynamoDBNull());
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void ToEntryTestBoolReturnsConvertedValue(bool input)
+        {
+            _sut.ToEntry(input).Should().BeEquivalentTo(new DynamoDBBool(input));
+        }
+
+        [Fact]
+        public void ToEntryTestInvalidInputThrows()
+        {
+            _sut.Invoking((c) => c.ToEntry("This is an error"))
+                .Should().Throw<InvalidCastException>();
+        }
+
+        [Fact]
+        public void FromEntryTestNullValueReturnsNull()
+        {
+            _sut.FromEntry(null).Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void FromEntryTestBoolReturnsConvertedValue(bool input)
+        {
+            DynamoDBEntry dbEntry = new DynamoDBBool(input);
+
+            ((bool) _sut.FromEntry(dbEntry)).Should().Be(input);
+        }
+
+        [Fact]
+        public void FromEntryTestInvalidInputThrows()
+        {
+            DynamoDBEntry dbEntry = new Primitive { Value = "This is an error" };
+
+            _sut.Invoking((c) => c.FromEntry(dbEntry))
+                .Should().Throw<FormatException>();
+        }
+    }
+}
---
+++ Hackney.Core/DynamoDb/Converters/DynamoDbBoolConverter.cs
@@ -0,0 +1,22 @@
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+
+namespace Hackney.Core.DynamoDb.Converters
+{
+    public class DynamoDbBoolConverter : IPropertyConverter
+    {
+        public DynamoDBEntry ToEntry(object value)
+        {
+            if (null == value) return new DynamoDBNull();
+
+            return new DynamoDBBool((bool) value);
+        }
+
+        public object FromEntry(DynamoDBEntry entry)
+        {
+            if (entry is null) return null;
+            if (entry is DynamoDBBool) return entry.AsBoolean();
+            return bool.Parse(entry.AsString());
+        }
+    }
+}
