308f9b99e92fdd27850326894ea641ebc151e5fe
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-07-05 14:51:39 +0100

    Added context wrapper

---
+++ Hackney.Core/Http/HttpContextWrapper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Hackney.Core.Http
+{
+    public class HttpContextWrapper : IHttpContextWrapper
+    {
+        public IHeaderDictionary GetContextRequestHeaders(HttpContext context)
+        {
+            return context.Request.Headers;
+        }
+    }
+}
---
+++ Hackney.Core/Http/IHttpContextWrapper.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Hackney.Core.Http
+{
+    public interface IHttpContextWrapper
+    {
+        IHeaderDictionary GetContextRequestHeaders(HttpContext context);
+    }
+}
