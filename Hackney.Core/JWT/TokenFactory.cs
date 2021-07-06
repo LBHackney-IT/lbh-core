c7d560b9c6d89f1dbfa4cc922d04f63d483aff5a
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-07-06 10:41:59 +0100

    Fixed bearer inconsistency

--- Hackney.Core/JWT/TokenFactory.cs
+++ Hackney.Core/JWT/TokenFactory.cs
@@ -1,4 +1,6 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
@@ -10,9 +12,10 @@ namespace Hackney.Core.JWT
         public Token Create(IHeaderDictionary headerDictionary)
         {
             var handler = new JwtSecurityTokenHandler();
-            var encodedStringToken = headerDictionary["Authorization"];
+            var encodedStringValueToken = headerDictionary["Authorization"];
+            var encodedString = encodedStringValueToken.ToArray().First().Replace("Bearer ", "", StringComparison.CurrentCultureIgnoreCase);
 
-            var jwtToken = handler.ReadJwtToken(encodedStringToken);
+            var jwtToken = handler.ReadJwtToken(encodedString);
             var decodedPayload = Base64UrlEncoder.Decode(jwtToken.EncodedPayload);
             return JsonConvert.DeserializeObject<Token>(decodedPayload);
         }
