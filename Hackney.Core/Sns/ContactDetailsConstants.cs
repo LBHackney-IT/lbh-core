f2f423dc0223e2cec7398c3fd91a590e084928f3
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-08-11 10:59:31 +0100

    Move into src & test folders

--- Hackney.Core/Sns/ContactDetailsConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Hackney.Shared.Sns
-{
-    public static class ContactDetailsConstants
-    {
-        public const string CREATED = "ContactDetailAddedEvent";
-        public const string DELETED = "ContactDetailDeletedEvent";
-    }
-}
