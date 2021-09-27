8c2b7a2701bf729090d564dd0bb28dcf20fcd8d0
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-09-27 11:50:39 +0100

    Add generic EventTypes

---
+++ Hackney.Core/Hackney.Core.Sns/EventTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace Hackney.Shared.Sns
+{
+    public static class EventTypes
+    {
+        public const string PersonCreatedEvent = "PersonCreatedEvent";
+        public const string PersonUpdatedEvent = "PersonUpdatedEvent";
+
+        public const string PersonAddedToTenureEvent = "PersonAddedToTenureEvent";
+        public const string PersonRemovedFromTenureEvent = "PersonRemovedFromTenureEvent";
+
+        public const string ContactDetailAddedEvent = "ContactDetailAddedEvent";
+        public const string ContactDetailDeletedEvent = "ContactDetailDeletedEvent";
+
+        public const string TenureCreatedEvent = "TenureCreatedEvent";
+        public const string TenureUpdatedEvent = "TenureUpdatedEvent";
+    }
+}
