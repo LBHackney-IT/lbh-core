a208d2ff6e9adc2961a9f1b8c6f63cfd23c97e87
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-10-04 14:04:59 +0100

    Add BaseSteps, BaseApiFixture & LogCallAspectFixture

---
+++ Hackney.Core/Hackney.Core.Testing/BaseSteps.cs
@@ -0,0 +1,31 @@
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Hackney.Core.Testing
+{
+    public class BaseSteps
+    {
+        protected readonly HttpClient _httpClient;
+
+        protected HttpResponseMessage _lastResponse;
+        protected readonly JsonSerializerOptions _jsonOptions;
+
+        public BaseSteps(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _jsonOptions = CreateJsonOptions();
+        }
+
+        protected JsonSerializerOptions CreateJsonOptions()
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+            options.Converters.Add(new JsonStringEnumConverter());
+            return options;
+        }
+    }
+}
---
+++ Hackney.Core/Hackney.Core.Testing/JsonOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Hackney.Core.Testing
+{
+    public static class JsonOptions
+    {
+        public static JsonSerializerOptions CreateJsonOptions()
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+            options.Converters.Add(new JsonStringEnumConverter());
+            return options;
+        }
+    }
+}
+
