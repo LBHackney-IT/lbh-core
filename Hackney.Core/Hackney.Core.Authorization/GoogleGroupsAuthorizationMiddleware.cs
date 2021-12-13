41eee2e00ac505f1379a8b592bc84f8176fb958a
Anna <golosowa.anna@gmail.com>
2021-12-13 20:29:44 +0300

    Add google groups auth (#39)
    * Add google groups authorization middleware
    
    * Code formatting
    
    * Move BaseErrorResponse to the separate class
    
    * Add package description and Readme file
    
    * Remove typo

---
+++ Hackney.Core/Hackney.Core.Authorization/GoogleGroupsAuthorizationMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using Hackney.Core.JWT;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Hackney.Core.Authorization
+{
+    public class GoogleGroupsAuthorizationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GoogleGroupsAuthorizationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, ITokenFactory tokenFactory)
+        {
+            var token = tokenFactory.Create(context.Request.Headers);
+            if (token == null)
+            {
+                await HandleResponseAsync(context, HttpStatusCode.Unauthorized, "JWT token cannot be parsed!").ConfigureAwait(false);
+                return;
+            }
+
+            if (token.Groups == null)
+            {
+                await HandleResponseAsync(context, HttpStatusCode.Forbidden, "JWT token should contain [groups] claim!").ConfigureAwait(false);
+                return;
+            }
+            var requiredGoogleGroupsVariable = Environment.GetEnvironmentVariable("REQUIRED_GOOGL_GROUPS");
+            if (requiredGoogleGroupsVariable == null)
+            {
+                await HandleResponseAsync(context, HttpStatusCode.InternalServerError, "Cannot resolve REQUIRED_GOOGL_GROUPS environment variable!").ConfigureAwait(false);
+                return;
+            }
+
+            var requiredGoogleGroups = requiredGoogleGroupsVariable.Split(';');
+            if (!token.Groups.Any(g => requiredGoogleGroups.Contains(g)))
+            {
+                await HandleResponseAsync(context, HttpStatusCode.Forbidden, "Forbidden").ConfigureAwait(false);
+                return;
+            }
+
+            await _next.Invoke(context).ConfigureAwait(false);
+        }
+
+        public async Task HandleResponseAsync(HttpContext context, HttpStatusCode code, string message)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            response.StatusCode = (int)code;
+
+            await response.WriteAsync(JsonConvert.SerializeObject(new BaseErrorResponse((int)code, message)))
+                   .ConfigureAwait(false);
+        }
+    }
+}
---
+++ Hackney.Core/Hackney.Core.Authorization/README.md
@@ -0,0 +1,10 @@
+﻿# Hackney.Core.Authorization NuGet Package
+Common library providing functionality to validates that provided in request JWT token contains correct Google Group. Used in LBH Web API services.
+
+## Usage
+
+To use this middleware you need to add the next line in your `Configure(...)` methon in `Startup.cs` file:
+    
+	    app.UseGoogleGroupAuthorization();
+	
+This middleware will use `REQUIRED_GOOGL_GROUPS` environment valiable to get required Google groups list
