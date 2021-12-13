c0100a502893a539f7676edd6f8638cacfac65e0
AnnaGolosova <golosowa.anna@gmail.com>
2021-12-13 19:52:10 +0300

    Move BaseErrorResponse to the separate class

--- Hackney.Core/Hackney.Core.Authorization/GoogleGroupsAuthorizationMiddleware.cs
+++ Hackney.Core/Hackney.Core.Authorization/GoogleGroupsAuthorizationMiddleware.cs
@@ -60,37 +60,4 @@ namespace Hackney.Core.Authorization
                    .ConfigureAwait(false);
         }
     }
-
-    public class BaseErrorResponse
-    {
-        public BaseErrorResponse() { }
-
-        public BaseErrorResponse(int statusCode, string message, string details = null)
-        {
-            StatusCode = statusCode;
-            Message = message;
-            Details = details ?? string.Empty;
-        }
-
-        /// <summary>
-        /// Status code
-        /// </summary>
-        /// <example>
-        /// 400
-        /// </example>
-        public int StatusCode { get; set; }
-
-        /// <summary>
-        /// Error message
-        /// </summary>
-        /// <example>
-        /// Model cannot be null
-        /// </example>>
-        public string Message { get; set; }
-
-        /// <summary>
-        /// Stack Trace of Exception
-        /// </summary>
-        public string Details { get; set; }
-    }
 }
