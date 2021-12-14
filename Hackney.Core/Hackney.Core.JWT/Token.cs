2a1a4e1ae5228c6107cfef02f34a431a9ea80579
AnnaGolosova <golosowa.anna@gmail.com>
2021-12-14 11:15:01 +0300

    Extent Ext claim to be long

--- Hackney.Core/Hackney.Core.JWT/Token.cs
+++ Hackney.Core/Hackney.Core.JWT/Token.cs
@@ -10,10 +10,10 @@
 
         public string Name { get; set; }
 
-        public int Nbf { get; set; }
+        public long Nbf { get; set; }
 
-        public int Exp { get; set; }
+        public long Exp { get; set; }
 
-        public int Iat { get; set; }
+        public long Iat { get; set; }
     }
 }
