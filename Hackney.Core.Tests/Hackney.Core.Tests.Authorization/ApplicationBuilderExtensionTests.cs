fee9499caa068b0fe7b021f2913820108872ca96
Anna <golosowa.anna@gmail.com>
2021-12-30 16:10:53 +0300

    Feature/create workflow for auth package (#43)
    * Add Hackney.Core.Authorization build step to the docker-compose
    
    * Fix Authorization publish pipeline
    
    * Try to add dependecy on JWT nuget
    
    * Add build of Hackney.Core.JWT
    
    * Fix, try 2
    
    * Fix, version 3
    
    * Fix, version 4
    
    * Fix, try 5
    
    * Fix, version 6
    
    * Extent Ext claim to be long
    
    * Add LBHPACKAGESTOKEN to JWT pipeline
    
    * Add new trigger for Authorization package
    
    * Feature/create test project for auth package (#42)
    
    * Add unit tests for authorization package.
    
    * Fix code formatting
    
    * Add workflow for authorization
    
    * Fix authorization workflow
    
    * Fix authorization test project path
    
    * Fix dockerfile for authorization workflow
    
    * Fix dockerfile for authorization workflow try #2
    
    * Fix dockerfile for authorization workflow try #3
    
    * Little code refactoring
    
    * Fix git versioning.
    
    * Revert "Fix git versioning."
    
    This reverts commit 5eb244cdf30eb7945074a2553d528509cdf85f62.

---
+++ Hackney.Core.Tests/Hackney.Core.Tests.Authorization/ApplicationBuilderExtensionTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using System;
+using Xunit;
+
+namespace Hackney.Core.Tests.Authorization
+{
+    public class ApplicationBuilderExtensionTests
+    {
+        [Fact]
+        public void UseGoogleGroupAuthorizationTestNullAppThrows()
+        {
+            IApplicationBuilder app = null;
+            
+            Action act = () => Hackney.Core.Authorization.ApplicationBuilderExtension.UseGoogleGroupAuthorization(app);
+            
+            act.Should().Throw<ArgumentNullException>().WithMessage("Value cannot be null. (Parameter 'IApplicationBuilder')");
+        }
+    }
+}
---
+++ Hackney.Core.Tests/Hackney.Core.Tests.Authorization/BaseErrorResponseTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Hackney.Core.Authorization;
+using System.Net;
+using Xunit;
+
+namespace Hackney.Core.Tests.Authorization
+{
+    public class BaseErrorResponseTests
+    {
+        [Theory]
+        [InlineData("Message", (int)HttpStatusCode.Forbidden, "Details", "Details")]
+        [InlineData("Message", (int)HttpStatusCode.Forbidden, null, "")]
+        public void BaseErrorResponseConstructorWithDifferentDetails(string expectedMessage, int expectedStatusCode, 
+            string actualDetails, string expectedDetails)
+        {
+            var sut = new BaseErrorResponse(expectedStatusCode, expectedMessage, actualDetails);
+
+            sut.Details.Should().Be(expectedDetails);
+            sut.Message.Should().Be(expectedMessage);
+            sut.StatusCode.Should().Be(expectedStatusCode);
+        }
+    }
+}
