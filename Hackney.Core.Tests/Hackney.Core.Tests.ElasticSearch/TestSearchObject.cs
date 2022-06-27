4d8d3729eaa7e5ee04e2382a0f4c2a92ee2a4da3
Dave Dean <david.dean@hackney.gov.uk>
2022-06-27 13:22:31 +0100

    Add tests

---
+++ Hackney.Core.Tests/Hackney.Core.Tests.ElasticSearch/TestSearchObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackney.Core.Tests.ElasticSearch
+{
+    public class TestSearchObject
+    {
+        // Generic class for testing QueryBuilder
+    }
+}
