2714706206036a035cffa25492f12c24fd06361b
duslerke <43747286+Duslerke@users.noreply.github.com>
2026-02-23 12:10:11 +0000

    Create presentation layer class for parsing Hackney Token.
    This is so that both the old token format, and the new Cognito token format both get supported at the same time w/o any changes to the domain logic.

---
+++ Hackney.Core/Hackney.Core.JWT/TokenPresentation.cs
@@ -0,0 +1,28 @@
+using Newtonsoft.Json;
+
+namespace Hackney.Core.JWT
+{
+    /// <summary>
+    /// Presentation layer object representing what Token could be.
+    /// A seperate object is needed to avoid breaking the existing
+    /// Domain layer contract. Do not use outside Hackney.Core.JWT,
+    /// hence 'internal'.
+    /// </summary>
+    internal class TokenPresentation
+    {
+        public string Sub { get; set; }
+
+        // The old token format groups field
+        public string[] Groups { get; set; }
+
+        // New cognito token format field
+        [JsonProperty("custom:groups")]
+        public string CustomGroups { get; set; }
+
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public long Nbf { get; set; }
+        public long Exp { get; set; }
+        public long Iat { get; set; }
+    }
+}
