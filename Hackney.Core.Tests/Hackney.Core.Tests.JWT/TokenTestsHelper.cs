fc6dc0c4bf0efa61ae72ca6031f68dc0a612906d
duslerke <43747286+Duslerke@users.noreply.github.com>
2026-02-23 16:14:45 +0000

    Add presentation and domain token object generators to jwt test helpers.

--- Hackney.Core.Tests/Hackney.Core.Tests.JWT/TokenTestsHelper.cs
+++ Hackney.Core.Tests/Hackney.Core.Tests.JWT/TokenTestsHelper.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
 using System.Linq;
+using AutoFixture;
+using Hackney.Core.JWT;
 
 namespace Hackney.Core.Tests.JWT
 {
     internal static class TokenTestsHelper
     {
         public static char CognitoTokenGoogleGroupsSeparator => ';';
+        private static Fixture _fixture = new Fixture();
 
         private static IEnumerable<string> GenerateGoogleGroups(int count)
         {
@@ -19,6 +22,20 @@ namespace Hackney.Core.Tests.JWT
             var googleGroups = GenerateGoogleGroups(count);
 
             return string.Join(CognitoTokenGoogleGroupsSeparator, googleGroups);
+        }
+
+        private static TokenPresentation GenerateTestTokenPresentationObj()
+        {
+            return _fixture.Build<TokenPresentation>()
+                .With(x => x.CustomGroups, GenerateCognitoTokenGroupsString(count: 5))
+                .Create();
+        }
+
+        private static Token GenerateTestTokenObj()
+        {
+            return _fixture.Build<Token>()
+                .With(x => x.Groups, GenerateGoogleGroups(count: 5))
+                .Create();
         }
     }
 }
